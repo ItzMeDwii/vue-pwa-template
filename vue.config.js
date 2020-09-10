574dbadd39258118dd5a7a150045d49296071835
Satya Bagus Dwiatmaja <dwii5359@azalelnation.com>
2020-09-11 01:05:50 +0700

    chore: add vue config

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    runtimeCompiler: true,
+    productionSourceMap: true,
+    configureWebpack: {
+      devtool: 'nosources-source-map'
+    },
+      pwa: {
+        name: 'Vue',
+        themeColor: '#212121',
+        msTileColor: '#212121',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'blue',
+        workboxOptions: {
+          skipWaiting: true,
+          clientsClaim: true
+      }
+      }
+    }
+  }
