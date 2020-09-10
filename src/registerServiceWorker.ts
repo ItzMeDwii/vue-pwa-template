805357e7190a92e9741868ae3fbccf290840ae1b
ItzMeDwii <dwii5359@azalelnation.com>
2020-09-10 13:36:45 +0700

    fix: service-worker

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -19,8 +19,12 @@ if (process.env.NODE_ENV === 'production') {
     updatefound () {
       console.log('New content is downloading.')
     },
-    updated () {
+    updated (registration) {
       console.log('New content is available; please refresh.')
+      registration.unregister()
+      setTimeout(() => {
+        window.location.reload(true)
+      }, 1000)
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
